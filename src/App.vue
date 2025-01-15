39480a470980bc7cc5755cb0bc66bc71cd9db46c
hujunjie <hujunjie@1token.trade>
2025-01-15 11:13:13 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>{{ store.state.count }}</h1>
+  <button @click="add">修改</button>
+  <button @click="addAsync">异步修改</button>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import { Options, Vue } from "vue-class-component";
+import { useStore } from "vuex";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-</script>
+@Options({})
+export default class App extends Vue {
+  private store = useStore();
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  private add() {
+    this.store.commit("add", 1);
+  }
+
+  private addAsync() {
+    this.store.dispatch("addAsync", 1);
+  }
+
+  mounted() {
+    console.log(this.store);
+  }
 }
-</style>
+</script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,24 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    count: 5,
   },
   getters: {
+    double(state) {
+      return state.count * 2;
+    },
   },
   mutations: {
+    add(state, payload) {
+      state.count += payload;
+    },
   },
   actions: {
+    addAsync({ commit }, payload) {
+      setTimeout(() => {
+        commit("add", payload);
+      }, 1000);
+    },
   },
-  modules: {
-  }
-})
+});
